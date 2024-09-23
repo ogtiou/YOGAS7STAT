dda50604d8353793362e04aeb27be0eb1d500fd7
Teo Raju <teoraju@Teo-MacBook-Pro.local>
2024-09-23 03:01:47 +0300

    third

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: '/your-repo-name',
+    trailingSlash: true,
     images: {
         unoptimized: true
     }
