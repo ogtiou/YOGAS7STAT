bdce841e7ede0bd2910576a48793dd0b9dbb5e31
Teo Raju <teoraju@Teo-MacBook-Pro.local>
2024-09-23 03:37:50 +0300

    stat

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    // basePath: '/YOGAS7STATIC',
+    basePath: '',
     trailingSlash: true,
     images: {
         unoptimized: true
