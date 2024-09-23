16c28b5101d5b7822d548872aa729df868f2884f
Teo Raju <teoraju@Teo-MacBook-Pro.local>
2024-09-23 04:37:40 +0300

    work

--- app/page.tsx
+++ app/page.tsx
@@ -106,8 +106,8 @@ export default function Home() {
                 </p>
             </div>
           </div>
-          <div className="flex-1 overflow-hidden flex items-center justify-center">
-            <div className="w-[90%] h-[30rem] mt-4 xl:mt-0 xl:h-full rounded-xl overflow-hidden">
+          <div className="flex-1 flex items-center justify-center z-50">
+            <div className="w-[90%] h-[30rem] mt-4 xl:mt-0 xl:h-full rounded-xl">
               <Image src={bali} alt="bali image" className="w-full h-full object-cover"/>
             </div>
           </div>
